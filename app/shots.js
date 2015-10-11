ade15fa66a884c3ef8c125daca9ff06db0076d17
czem <caioadz@hotmail.com>
2015-10-11 16:49:35 -0300

    Dribbble shots

--- app/shots.js
+++ app/shots.js
@@ -6,13 +6,27 @@ angular.module('Shots', ['ngRoute'])
 	
 }])
 
-.controller('ShotsCtrl', function(ShotsResource, $scope, $modal, $location, $http) {
+.controller('ShotsCtrl', function(ShotsResource, $scope, $uibModal, $location, $http) {
+	
+	$scope.shots = [];
+	
+	/*$scope.shots.push({
+		likes_count: 100,
+		views_count: 300,
+		comments_count: 1000,
+		images: {
+			normal: 'images/pois_1x.png'
+		}
+	})*/
 	
 	ShotsResource.query().$promise.then(function(data) {
 		$scope.shots = data;
 	});
 	
 	$scope.openModal = function () {
-		$modal.open({templateUrl: 'details'});
+		$uibModal.open({
+			templateUrl: 'details',
+			size: 'details'
+		});
 	}
 });
