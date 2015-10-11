2756656fc4a914270543cbbdf22fe5f46d163773
czem <caioadz@hotmail.com>
2015-10-11 15:56:38 -0300

    OAuth2

--- app/shots.js
+++ app/shots.js
@@ -31,7 +31,7 @@ angular.module('Shots', ['ngRoute'])
 	console.log('wow!');
 	$http.get('//api.dribbble.com/v1/user?access_token=251eccbc923c4d792366411d7587058eb2a4527dc7fcf2380879172479106e6f');
 	
-	//console.log($scope.test = ShotsResource.query());
+	console.log($scope.test = ShotsResource.query());
 	
 	$scope.openModal = function () {
 		$modal.open({templateUrl: 'details'});
