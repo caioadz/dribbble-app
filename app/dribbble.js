a1ac9af910eb0d0fdc52a3aa05bdc6039ef78e37
czem <caioadz@hotmail.com>
2015-10-11 20:09:10 -0300

    Dribbble shots

--- app/dribbble.js
+++ app/dribbble.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var client_id = '251eccbc923c4d792366411d7587058eb2a4527dc7fcf2380879172479106e6f';
-var api_url = '//api.dribbble.com/v1{relative_url}?per_page=30&access_token=' + client_id; 
+var api_url = '//api.dribbble.com/v1{relative_url}?per_page=60&access_token=' + client_id; 
 
 angular.module('Dribbble', [])
 
