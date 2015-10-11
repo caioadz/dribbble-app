a611c228c0cb340aca94fbd367290577e2bc6b7c
czem <caioadz@hotmail.com>
2015-10-11 16:08:53 -0300

    Dribbble shots

--- app/shots.js
+++ app/shots.js
@@ -28,10 +28,7 @@ angular.module('Shots', ['ngRoute'])
 		comments: 1
 	});
 	
-	console.log('wow!');
-	$http.get('//api.dribbble.com/v1/user?access_token=251eccbc923c4d792366411d7587058eb2a4527dc7fcf2380879172479106e6f');
-	
-	console.log($scope.test = ShotsResource.query());
+	$scope.shots = ShotsResource.query();
 	
 	$scope.openModal = function () {
 		$modal.open({templateUrl: 'details'});
