340992ad7932076504d7a6926c98810fb834690e
czem <caioadz@hotmail.com>
2015-10-11 19:02:34 -0300

    Dribbble shots

--- app/details.js
+++ app/details.js
@@ -4,5 +4,5 @@ angular.module('Details', ['ngRoute'])
 
 .controller('DetailsCtrl', function($scope, $sce, shot) {
 	$scope.shot = shot;
-	$scope.shot.description = $sce.trustAsHtml($scope.shot.description);
+	$scope.shot.description = $sce.trustAsHtml($scope.shot.description || '');
 });
